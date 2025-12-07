e52a19b893e8ee0cce7ddabfb18777e0b989b9a3
Manuel Alejandro <manuelalejandro@MacBook-Air-de-Manuel.local>
2025-12-06 20:48:45 -0500

    Solución de vulnerabilidad y errores de compilación de Next.js 14

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */ // Puedes mantener el JSDoc para tipado
+const nextConfig = {
+  // ... tu configuración
+};
+
+module.exports = nextConfig; // ¡Usa module.exports!
